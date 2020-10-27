4ac966c858d5b2f4669a36b3e34dad4e9d63dfd6
KC Tung <34718678+shinchan75034@users.noreply.github.com>
2020-10-26 19:38:50 -0500

    Add files via upload

---
+++ chapter_06/localtuningwork/tuner_driver.sh
@@ -0,0 +1,6 @@
+python3 hp_kt_resnet_local_pub.py \
+--model_dir=resnet_local_hb_output  \
+--cached_basemodel_dir=imagenet_resnet_v2_50_feature_vector_4 \
+--train_epoch_best=2 \
+--tuner_type=hypoerband
+
